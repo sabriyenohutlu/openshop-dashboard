0473c6ac5169934e768dcd72e06d605b25c234f6
sabriyeuregen <sabriyeuregen@gmail.com>
2024-08-04 16:17:02 +0300

    Added : .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvPn-QWmnSSksRZQt5N8SBndKE6hpsJRk",
-  authDomain: "openshop-3210e.firebaseapp.com",
-  projectId: "openshop-3210e",
-  storageBucket: "openshop-3210e.appspot.com",
-  messagingSenderId: "905042212200",
-  appId: "1:905042212200:web:eaacc53640428d8ec7d2dd"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
